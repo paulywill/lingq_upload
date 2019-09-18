604de5281e2a48b111ea1706e1513d832012be19
Paul Gamble <pgamble@gmail.com>
2019-09-17 23:10:50 -0400

    ⚡️ Working flawlessly
    MVP / objective reached

--- upload.py
+++ upload.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-#import eyed3
+from os.path import isfile, join
+from os import listdir
 import sys
 import requests
 import os
@@ -9,34 +10,31 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 load_dotenv()
 key = os.getenv("APIKey")
-#mp3DIR = os.getenv("mp3DIR")
-#status = os.getenv("status")
-#collectionID = os.getenv("collectionID")
-
-
-
-
-
+postAddress = os.getenv("postAddress")
+mypath = os.getenv("mypath")
+mp3DIR = os.getenv("mp3DIR")
+status = os.getenv("status")
+collectionID = os.getenv("collectionID")
 
 
 def __main__():
-    for args in sys.argv[1:]:
-
-        title = args 
-        transcript = ''
-
+    # Create list/array of mp3
+    mp3s = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    mp3s.sort(reverse=False)
+    for mp3 in mp3s:
+       
         m = MultipartEncoder([
-                    ('title',title),
-                    ('text',transcript),
-                    ('status','private'),
-                    ('collection','495095'),
-                    ('audio', (args, open(args, 'rb'), 'audio/mpeg'))]   
+                    ('title', mp3[0:11]),
+                    ('text', mp3[0:11]),
+                    ('status',status),
+                    ('collection',collectionID),
+                    ('audio', (mp3, open(mypath + '/' + mp3, 'rb'), 'audio/mpeg'))]   
                 )
 
         h = {'Authorization': key,
             'Content-Type': m.content_type}
 
-        r = requests.post('https://www.lingq.com/api/v2/fr/lessons/', data=m, headers=h)
+        r = requests.post(postAddress, data=m, headers=h)
         print(r.status_code)
         print(r.text)
 
